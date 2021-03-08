d882a441e82d23d6e77dcadeb406e963d2ee2c59
Dawid Kolcz <dawid.kolcz@microsoft.wsei.edu.pl>
2021-03-08 21:14:40 +0100

    Finished minimum.

--- lab1/index.js
+++ lab1/index.js
@@ -1,22 +1,81 @@
 var App = /** @class */ (function () {
     function App() {
-        this.getRef();
-        this.watchData();
+        this.startingInputs = 4;
+        this.inputArray = [];
+        this.outputArray = [];
+        this.resultArray = [];
+        this.main();
     }
-    App.prototype.getRef = function () {
-        this.input1 = document.querySelector("#input1");
-        this.output1 = document.querySelector("#output1");
+    App.prototype.main = function () {
+        this.inputDiv = document.querySelector(".input-data");
+        this.outputDiv = document.querySelector(".output-data");
+        this.createDefaultInputs(this.startingInputs);
+        this.createAddMoreButton();
+        this.createOutputs();
     };
-    App.prototype.getData = function () {
-        this.a = +this.input1.value;
+    App.prototype.createDefaultInputs = function (cellCount) {
+        for (var i = 0; i < cellCount; i++) {
+            this.createInput();
+        }
+    };
+    App.prototype.createInput = function () {
+        var _this = this;
+        var newInput = document.createElement("input");
+        newInput.type = "number";
+        newInput.addEventListener("change", function () { return _this.displayData(); });
+        this.inputArray.push(newInput);
+        this.inputDiv.appendChild(newInput);
+    };
+    App.prototype.createAddMoreButton = function () {
+        var _this = this;
+        var addInputDiv = document.querySelector(".add-input-data");
+        var button = document.createElement("input");
+        button.value = "Add more inputs";
+        button.type = "button";
+        button.addEventListener("click", function () { return _this.createInput(); });
+        addInputDiv.appendChild(button);
+    };
+    App.prototype.createOutputs = function () {
+        for (var i = 0; i < 4; i++) {
+            var newOutput = document.createElement("input");
+            newOutput.type = "text";
+            newOutput.disabled = true;
+            this.outputArray.push(newOutput);
+            this.outputDiv.appendChild(newOutput);
+        }
+    };
+    App.prototype.getValues = function () {
+        var arr = [];
+        for (var _i = 0, _a = this.inputArray; _i < _a.length; _i++) {
+            var i = _a[_i];
+            arr.push(+i.value);
+        }
+        return arr;
     };
     App.prototype.processData = function () {
-        this.getData();
-        this.output1.value = this.a.toString();
+        //sum, avg, min, max
+        var arr = this.getValues();
+        var sum = 0;
+        var avg = 0;
+        var min = 0;
+        var max = 0;
+        for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+            var i = arr_1[_i];
+            sum += i;
+        }
+        avg = sum / arr.length;
+        min = Math.min.apply(Math, arr);
+        max = Math.max.apply(Math, arr);
+        this.resultArray[0] = sum;
+        this.resultArray[1] = avg;
+        this.resultArray[2] = min;
+        this.resultArray[3] = max;
     };
-    App.prototype.watchData = function () {
-        var _this = this;
-        this.input1.addEventListener('input', function () { return _this.processData(); });
+    App.prototype.displayData = function () {
+        this.processData();
+        for (var i = 0; i < 4; i++) {
+            this.outputArray[i].value = this.resultArray[i].toString();
+        }
     };
     return App;
 }());
--- lab1/index.ts
+++ lab1/index.ts
@@ -1,48 +1,95 @@
 class App{
-	startingTable = 4;
-	inputArray: Array<HTMLInputElement>;
+	startingInputs: number = 4;
+	inputDiv: HTMLElement;
+	outputDiv: HTMLElement;
 
-	input1: HTMLInputElement;
-	input2: HTMLInputElement;
-	input3: HTMLInputElement;
-	input4: HTMLInputElement;
-
-	output1: HTMLInputElement;
-        output2: HTMLInputElement;
-        output3: HTMLInputElement;
-        output4: HTMLInputElement;
-
-	a: decimal;
+	inputArray: Array<HTMLInputElement> = [];
+	outputArray: Array<HTMLInputElement> = [];
 	
+	resultArray: Array<number> = [];
+
 	constructor(){
-		this.createTable(startingTable);
-		this.getRef();
-		this.watchData();
+		this.main();
+	}
+
+	main(): void{
+		this.inputDiv = document.querySelector(".input-data");
+		this.outputDiv = document.querySelector(".output-data");
+		this.createDefaultInputs(this.startingInputs);
+		this.createAddMoreButton();
+		this.createOutputs();
 	}
 	
-	createTable(cellCount: decimal): void{
-		for(let i = 0 i < cellCount; i++){
-			
-		}	
+	createDefaultInputs(cellCount: number): void{
+		for(let i = 0; i < cellCount; i++){
+			this.createInput();
+		}
+	}
+	
+	createInput(): void{
+		const newInput = document.createElement("input")! as HTMLInputElement;
+		newInput.type = "number";
+		newInput.addEventListener("change", () => this.displayData());
+		this.inputArray.push(newInput);
+		this.inputDiv.appendChild(newInput);
 	}
 
-	getRef(): void{	
-		this.input1 = document.querySelector("#input1");
-		
-		this.output1 = document.querySelector("#output1");
+	createAddMoreButton(): void{
+		const addInputDiv = document.querySelector(".add-input-data")! as HTMLElement;
+		const button = document.createElement("input")! as HTMLButtonElement;
+		button.value = "Add more inputs";
+		button.type = "button";
+		button.addEventListener("click", () => this.createInput());
+		addInputDiv.appendChild(button);
 	}
 
-	getData(): void{
-		this.a = +this.input1.value;
+	createOutputs(): void{
+		for(let i = 0; i < 4; i++){
+			const newOutput = document.createElement("input")! as HTMLInputElement;
+			newOutput.type = "text";
+			newOutput.disabled = true;
+			this.outputArray.push(newOutput);
+			this.outputDiv.appendChild(newOutput);
+		}
+	}
+	
+	getValues(): Array<number>{
+		let arr: Array<number> = [];
+		for(let i of this.inputArray){
+			arr.push(+i.value);
+		}
+		return arr;
 	}
 
 	processData(): void{
-		this.getData();
-		this.output1.value = this.a.toString();
-	}
+		//sum, avg, min, max
+		const arr: Array<number> = this.getValues();
 
-	watchData(): void{
-		this.input1.addEventListener('input', () => this.processData());
+		let sum = 0;
+		let avg = 0;
+		let min = 0;
+		let max = 0;
+
+		for(let i of arr){
+			sum += i;
+		}
+		
+		avg = sum/arr.length;
+		min = Math.min(...arr);
+		max = Math.max(...arr);
+		
+		this.resultArray[0] = sum;
+		this.resultArray[1] = avg;
+		this.resultArray[2] = min;
+		this.resultArray[3] = max;
+	}
+	
+	displayData(): void{
+		this.processData();
+
+		for(let i = 0; i < 4; i++){
+			this.outputArray[i].value = this.resultArray[i].toString();
+		}
 	}
 }	
 
