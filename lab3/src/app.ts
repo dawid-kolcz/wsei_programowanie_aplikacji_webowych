65e99eeb4c97291b1ae09a78790b3586c28cd32d
Dawid Kolcz <dawid.kolcz@microsoft.wsei.edu.pl>
2021-04-11 16:06:31 +0200

    webpack is broken

--- lab3/src/app.ts
+++ lab3/src/app.ts
@@ -1,3 +1,34 @@
 export class App {
-    
+    opwApiKey = '50d53005c0fd5f556bb4ef15224c4209';
+
+    constructor() {
+        this.getCityInfo('krakow');
+    }
+
+    async getCityInfo(city: string) {
+        const weather = await this.getWeather(city);
+        this.saveData(weather);
+    }
+
+    async getWeather(city: string): Promise<any> {
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+        return weatherData;
+    }
+
+    saveData(data: Promise<any>) {
+        console.log(data);
+        localStorage.setItem('weatherData', JSON.stringify(data));
+    }
+
+    getData() {
+        const data = localStorage.getItem('weatherData');
+        if (data) {
+            return JSON.parse(data);
+        } else {
+            return {};
+        }
+    }
 }
