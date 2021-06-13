13694940db295989223d2291140a5b6929c73236
dawid-kolcz <dawid.kolcz@microsoft.wsei.edu.pl>
2021-06-13 16:12:02 +0200

    Last push

--- NoteKeeper2/src/helpers/Functions.ts
+++ NoteKeeper2/src/helpers/Functions.ts
@@ -1,8 +1,24 @@
+import { Note } from "../objects/Notes";
+
 export * as Functions from "../helpers/Functions"
 
-export function creteNote(title: string, content: string, bgColor: string, pinned: boolean){
-    const noteDiv = document.querySelector("#noteDiv") as HTMLDivElement;
+export function saveNote(note: Note){
+    const notesWrapper = document.querySelector("#notesWrapper") as HTMLDivElement;
+    const pinnedWrapper = document.querySelector("#pinnedWrapper") as HTMLDivElement;
+
+}
+
+export function newNote(): void{
+    const mainWrapper = document.querySelector("#mainWrapper") as HTMLDivElement;
     const newNote = document.createElement("div") as HTMLDivElement;
 
-    
+    newNote.className = "newNote";
+
+    const newTitle = document.createElement("input") as HTMLInputElement; 
+    const newContent = document.createElement("input") as HTMLInputElement;
+
+    newNote.appendChild(newTitle);
+    newNote.appendChild(newContent);
+
+    mainWrapper.appendChild(newNote);
 }
--- NoteKeeper2/src/index.ts
+++ NoteKeeper2/src/index.ts
@@ -1,5 +1,14 @@
 import './main.scss';
 import  * as FB from "./objects/FB";
-import { creteNote, Functions } from "./helpers/Functions"
+import { Functions } from "./helpers/Functions"
 import { Note } from "./objects/Notes";
 
+function main(): void{
+    const addButton = document.querySelector("#addNoteButton") as HTMLButtonElement;
+    addButton.addEventListener("click", () => { 
+        
+        console.log("Create new note");
+    });
+}
+
+main();
